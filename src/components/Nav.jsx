c8b01fa155363d99b9003568703f72e86d4df900
aryan <gargaryan12122005@gmail.com>
2025-06-21 21:04:06 +0530

    home fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 function Nav() {
   return (
     <div className="flex justify-center items-center gap-6 p-5 bg-amber-500  ">
-        <NavLink to="/home" className={(e)=>(e.isActive?"text-orange-700":"text-white")}>Home</NavLink>
+        <NavLink to="/" className={(e)=>(e.isActive?"text-orange-700":"text-white")}>Home</NavLink>
         <NavLink to="/recipe" className={(e)=>(e.isActive?"text-orange-700":"text-white")}>Recipe</NavLink>
         <NavLink to="/create" className={(e)=>(e.isActive?"text-orange-700":"text-white")}>Create Recipe</NavLink>
     </div>
