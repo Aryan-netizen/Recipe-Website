13919e57216b7175a9c43768d4ce8d616ac441ef
aryan <gargaryan12122005@gmail.com>
2025-06-21 19:06:03 +0530

    main jsx fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,22 @@ import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import Wrapper from './utils/wrapper.jsx'
 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Wrapper from './utils/wrapper.jsx'; // Make sure this path is correct
+
 createRoot(document.getElementById('root')).render(
-  <Wrapper>
-    <BrowserRouter>
-    <App />
-    <ToastContainer/>
-    </BrowserRouter>
+  <StrictMode>
+    <Wrapper>
+      <BrowserRouter>
+        <App />
+        <ToastContainer />
+      </BrowserRouter>
     </Wrapper>
-)
+  </StrictMode>
+);
+
